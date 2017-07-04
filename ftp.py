6ffa681e3109a3e7ab0c3a87837608ff32abdf35
Sherry <sjslin@gmail.com>
2017-07-04 14:26:08 +0800

    previous ftp.py wrong, added correct one

--- ftp.py
+++ ftp.py
@@ -1,26 +1,33 @@
 from ftplib import FTP
-import sys
 
-def grabFile():
-    filename = 'example.txt'
-    localfile = open(filename, 'wb')
-    ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+
+def getFile():
+    filename = input('What file do you want to get? ')
+    localfile = open(filename, 'w+b')
+    ftp.retrbinary('RETR ' + filename, localfile.write, 1024)  # write in 1024-byte chunks
     ftp.quit()
+    print('In the file is: ')  # see what's in the file you got
+    localfile.flush()
+    localfile.seek(0)
+    print(localfile.read().decode('ascii'))
     localfile.close()
 
-def placeFile():
-    filename = 'exampleFile.txt'
+
+def storeFile():
+    filename = input('What file do you want to store? ')
     ftp.storbinary('STOR '+filename, open(filename, 'rb'))
     ftp.quit()
+    print('Stored')
+
 
 def main():
-	print 'Hello'
-	#domain name or server ip:
-	ftp = FTP('127.0.0.1')
-	ftp.login()
-	# ftp.login(user='username', passwd = 'password')
-	placeFile()
-	grabFile()
+    print('Hello')
+    ip = input("Enter a domain name or server IP address: ")
+    global ftp
+    ftp = FTP(ip)
+    ftp.login(user='hello', passwd='hello')
+    storeFile()
+    # getFile()
 
 if __name__ == '__main__':
-	main()
+    main()
