cffd91ab5c7cd3df4cf6f163d15703c1f6201b66
kawaiicheese <josephlu116@gmail.com>
2017-07-05 10:20:56 +0800

    change documentation to doxygen

--- app/ftp.py
+++ app/ftp.py
@@ -1,7 +1,7 @@
-##  @class: ftp.py
-#   @authors:
-#   @functions: getFile     recieves 1 specified file from ftp
-#               storeFile   stores 1 specified file to ftp
+##  @file ftp.py
+#   @title ftp
+#   @author
+#   @date 7/5/2017
 
 import os
 from ftplib import FTP
@@ -11,13 +11,13 @@ _ip = "localhost"    #put ip address
 _user = "local"      #put username
 _passwd = "j"        #put password
 
-##  @functionName:  getFile
-#   @description:   looks into FTP given by the user for a file
-#                   ftp credentials can be predefined above
-#   @parameters:    filename   name of file to look for
-#                   ip         ip address of ftp
-#                   user       user account of ftp
-#                   passwd     password of ftp user
+##  @brief  getFile gets a file from FTP
+#   @detail looks into FTP given by the user for a file
+#           ftp credentials can be predefined above
+#   @param  filename    name of file to look for
+#   @param  ip          ip address of ftp
+#   @param  user        user account of ftp
+#   @param  passwd      password of ftp user
 def getFile(filename, ip = _ip, user = _user, passwd = _passwd):
     #connect to ftp
     ftp = FTP(ip, user, passwd)
@@ -31,13 +31,13 @@ def getFile(filename, ip = _ip, user = _user, passwd = _passwd):
     ftp.quit()
     localfile.close()
 
-##  @functionName:  storeFile
-#   @description:   stores a file into FTP given by the user
-#                   ftp credentials can be predefined above
-#   @parameters:    filename   name of file to store into ftp
-#                   ip         ip address of ftp
-#                   user       user account of ftp
-#                   passwd     password of ftp user
+##  @brief  storeFile saves a file in ftp
+#   @detail stores a file into FTP given by the user
+#           ftp credentials can be predefined above
+#   @param  filename   name of file to store into ftp
+#   @param  ip         ip address of ftp
+#   @param  user       user account of ftp
+#   @param  passwd     password of ftp user
 def storeFile(filename, ip = _ip, user = _user, passwd = _passwd):
     ftp = FTP(ip, user, passwd)
     ftp.storbinary('STOR '+ filename, open(filename, "rb"))
