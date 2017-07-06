0d574674834449d7a629c73aecb75124de8918f2
kawaiicheese <josephlu116@gmail.com>
2017-07-06 17:34:53 +0800

    FrontEnd version 1

---
+++ app/unitTest.py
@@ -0,0 +1,24 @@
+## @file unitTest.py
+#  @title unitTest
+#  @author 
+#  @date 7/6/2017
+import unittest
+
+# The idea of this file is to test the file transfers
+# and other potential errors that may occur
+
+# Unifinished 
+class Test(unittest.TestCase):
+
+    #setup will set same beginning parameters
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    #testcases here
+
+
+
+if __name__ == '__main__':
+    unittest.main()
