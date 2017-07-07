85f72ca250f1e3b23e83e583a9661cc4a7ee2a0a
kawaiicheese <josephlu116@gmail.com>
2017-07-07 14:34:57 +0800

    frontend

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@
 
 from flask import Flask, render_template, request
 import ftp
-import database
+#import database
 
 app = Flask(__name__)
 
@@ -17,9 +17,9 @@ app = Flask(__name__)
 @app.route('/send', methods=['GET', 'POST'])
 def send():
     if request.method == 'POST':
-        file = request.form['fileToUpload']
+        file = request.form.get('fileToUpload')
         ftp.storeFile(file)
-        database.writeToDB(file)
+        #database.writeToDB(file)
         return render_template('fileupload.html', file=file)
     return render_template('index.html')
 
