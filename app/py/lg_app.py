cf54e5b58e4ced560adfb026f8411f35d6408917
timothylu <timothy.e.lu@gmail.com>
2017-07-26 01:42:34 -0700

    generate lattice and convert to x3d

--- app/py/lg_app.py
+++ app/py/lg_app.py
@@ -2,9 +2,105 @@ import sys
 import os
 from os.path import dirname, abspath
 import file_conversion
+import msh_to_x3d
+
+def read_nodes(job_id, nodes):
+    with open("lc_uploads/node_uploads/" + job_id + ".txt") as node_file:
+        for line in node_file:
+            if not line.strip():
+                continue
+            else:
+                words = line.split()
+                if is_number(words[0]):
+                    nodes.append(Node(int(words[0]),float(words[1]), float(words[2]), float(words[3])))
+    node_file.close()
+    return nodes
+
+def read_elements(job_id, elements):
+    global ELEMENT_ATTRIBUTES
+    flag = False
+    with open("lc_uploads/element_uploads/" + job_id + ".txt") as element_file:
+        for line in element_file:
+            if not line.strip():
+                continue
+            else:
+                words = line.split()
+                if is_number(words[0]):
+                    if not flag:
+                        #ELEMENT_ATTRIBUTES = words[1] + " " + words[2] + " " + words[3] + " " + words[4]
+                        ELEMENT_ATTRIBUTES = "2 2 3 4"
+                        flag = True
+                    elements.append(Element(int(words[6]), int(words[7]), int(words[8])))
+    element_file.close()
+    return elements
+
+def direction_delta(nodes):
+    maxX = max(node.x for node in nodes)
+    minX = min(node.x for node in nodes)
+    maxY = max(node.y for node in nodes)
+    minY = min(node.y for node in nodes)
+    maxZ = max(node.z for node in nodes)
+    minZ = min(node.z for node in nodes)
+    return Node(0, maxX - minX, maxY - minY, maxZ - minZ)
+
+def generate_lattice(job_id, nodes, elements, total_nodes, displacement_factor, x, y, z):
+    model = Lattice(elements, nodes)
+
+    output = open("lc_output/" + job_id + ".msh","w")
+    output.write("$MeshFormat\n")
+    # MESH FORMAT
+    output.write("2.2 0 8\n")
+    output.write("$EndMeshFormat\n$Nodes\n")
+    # WRITE NODES HERE
+    # NUM OF NODES
+    output.write(str(((x) * (y) * (z)) * len(model.nodes)) + "\n")
+
+    multiplier = 0
+    count = 0
+
+    for num1 in range(0,x):
+        for num2 in range (0,y):
+            for num3 in range(0,z):
+                count +=1
+                for n in model.nodes:
+                    output.write(str(n.idx + total_nodes * multiplier) +
+                                 " " + str(n.x + num1 * displacement_factor.x) +
+                                 " " + str(n.y + num2 * displacement_factor.y) +
+                                 " " + str(n.z + num3 * displacement_factor.z) + '\n')
+                multiplier += 1
+    
+    # Elements
+    output.write("$EndNodes\n$Elements\n")
+
+    output.write(str((x * y * z) * len(model.elements)) + "\n")
+    multiplier = 0
+    count = 0
+    index = 0
+    for num1 in range(0,x):
+        for num2 in range (0,y):
+            for num3 in range(0,z):
+                for e in model.elements:
+                    index += 1
+                    count += 1
+                    output.write(str(index) + " " + ELEMENT_ATTRIBUTES + 
+                                       " " + str(e.n1 + multiplier * total_nodes) + 
+                                       " " + str(e.n2 + multiplier * total_nodes) + 
+                                       " " + str(e.n3 + multiplier * total_nodes) + '\n')
+                multiplier +=1
+    output.write("$EndElements\n")
+    output.close()
+
+def main(job_id, x, y, z):
+    nodes = read_nodes(job_id, [])
+    total_nodes = nodes[len(nodes) - 1].idx
+    elements = read_elements(job_id, [])
+    displacement_factor = direction_delta(nodes)
+    generate_lattice(job_id, nodes, elements, total_nodes, displacement_factor, x, y, z)
+    msh_to_x3d.mshTox3d("lc_output/" + job_id + ".msh")
 
 if __name__ == "__main__":
     job_id = sys.argv[1]
     x = sys.argv[2]
     y = sys.argv[3]
     z = sys.argv[4]
+    main(job_id, x, y, z)
---
+++ app/py/msh_to_x3d.py
@@ -0,0 +1,81 @@
+import sys
+from os.path import exists
+
+_nodes = []
+_triangles = []
+_lines = set()
+
+# requires consecutive node numbers
+def readFile(msh_file):
+    readingNodes = False
+    nodesCounter = 0
+
+    readingElements = False
+    elementsCounter = 0
+    with open(msh_file) as f:
+        for line in f:
+            if  line.startswith('$Nodes'):
+                readingNodes = True
+            elif line.startswith('$Elements'):
+                readingElements = True
+            elif line.startswith('$EndNodes'):
+                readingNodes = False
+            elif line.startswith('$EndElements'):
+                readingElements = False
+            if readingNodes:
+                if nodesCounter > 1:
+                    keys = line.split()
+                    x = (float(keys[1]), float(keys[2]), float(keys[3]))
+                    _nodes.append(x)
+
+                nodesCounter += 1
+            elif readingElements:
+                if elementsCounter > 1:
+                    keys = line.split()
+                    if keys[1] == str(2) and len(keys) == 8:
+                        # get triangle vertex node numbers and put tuple in list
+                        tri_tuple = (int(keys[5]) - 1, int(keys[6]) - 1, int(keys[7]) - 1)
+                        _triangles.append(tri_tuple)
+                elementsCounter += 1
+
+    # for every triangle, add 3 lines in line set
+    # use frozensets so (a, b) is equivalent to (b, a)
+    for triangle in _triangles:
+        line = frozenset((triangle[0], triangle[1]))
+        _lines.add(line)
+        line = frozenset((triangle[1], triangle[2]))
+        _lines.add(line)
+        line = frozenset((triangle[0], triangle[2]))
+        _lines.add(line)
+
+def mshTox3d(filename):
+    readFile(filename)
+    # read stuff into file
+    f = open('C:\hello.x3d', 'w')
+    # write header (taken from PythonOCC's Visualization/Tesselator.cpp)
+    f.write("<?xml version='1.0' encoding='UTF-8'?>")
+    f.write("<!DOCTYPE X3D PUBLIC 'ISO//Web3D//DTD X3D 3.1//EN' 'http://www.web3d.org/specifications/x3d-3.1.dtd'>")
+    f.write("<X3D>")
+    f.write("<Head>")
+    f.write("<meta name='generator' content='pythonOCC, http://www.pythonocc.org'/>")
+    f.write("</Head><Scene>\n")
+    f.write("<Shape><Appearance><Material DEF='Shape_Mat' /></Appearance>\n")
+    f.write("<IndexedLineSet coordIndex='")
+    # iterate over set, adding lines to indexed line set
+    for line in _lines:
+        for end in line:
+            f.write(str(end) + ' ')
+        f.write(str(-1) + ' ')
+    f.write("'>\n<Coordinate point='")
+    # iterate over nodes
+    for node in _nodes:
+        f.write(str(node[0]) + ' ' + str(node[1]) + ' ' + str(node[2]) + ' ')
+    f.write("'/></IndexedLineSet>\n")
+    f.write("</Shape>\n")
+    f.write("</Scene></X3D>\n")
+    f.close()
+
+
+if __name__ == "__main__":
+   mshTox3d('C:/Users/MD580/Downloads/lattice.msh')
+
