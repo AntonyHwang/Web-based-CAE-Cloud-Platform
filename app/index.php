3357571d67d92a4315a3cf8883ff91060dc7e836
Antony Hwang <AntonyHwang@hotmail.com>
2017-08-11 08:36:40 +0800

    loader added

--- app/index.php
+++ app/index.php
@@ -6,19 +6,54 @@
 ?>
 
 <script type="text/javascript">
-    $(document).ready(function(){
-        $("input").click(function(){
-            $('row').css("filter","blur(3px)");
-            $.ajax({
-                type: "POST",
-                url: "generate_lattice.php",
-                data: { param: text}
-                }).done(function( o ) {
-                // do something
-                });
+    // $(document).ready(function(){
+    //     $('#submit').click(function(){
+    //         $('row').css("filter","blur(5px)");
+    //         $('#loader').removeAttr("hidden");
+    //     });
+    function calc_lattice() {
+        if ($('#x').val() === "" && $('#y').val() === "" && $('#z').val() === "") {
 
-        });
-    });
+        } else {
+            $('#loader').removeAttr("hidden");
+            $('row').css("filter","blur(5px)");
+        }
+
+        
+        // $.ajax({
+        //     type: 'POST',
+        //     url:'mesh_check.php',
+        //     dataType: 'json',
+        //     data: {
+        //         job_id: $('#id').val(), 
+        //         element_size: $('#sel1').val(),
+        //         max_element_size: $('#maxElementSize').val(),
+        //         min_element_size: $('#minElementSize').val()
+        //     },
+        //     success: function (data){
+        //         $('#animation').attr("hidden", "hidden");
+        //         $('.row').css("filter","blur(0px)");
+        //         if (data.conversion === "success") {
+        //             $('#submit').removeAttr("hidden");
+        //             alert("Succesfully meshed your model.");
+        //             displayMesh();
+        //         } else {
+        //             alert("Sorry, this file cannot be meshed due to unconnected nodes. Please upload another file.")
+        //         }
+        //     }
+        // });
+	}
+            // $.ajax({
+            //     type: "POST",
+            //     url: "generate_lattice.php",
+            //     dataType: 'json',
+            //     data: {
+            //         shape: $('#shape').val(), 
+            //         d_x: $('#x').val(),
+            //         d_y: $('#y').val(),
+            //         d_z: $('#z').val()
+            //     }
+            // });
 </script>  
 
 <html>
@@ -28,22 +63,22 @@
             <form action="index.php" method="post" enctype="multipart/form-data"> 
                 <h1>Lattice Generator</h1>
                 <h5>Shape:</h5>
-                <select name="element_size" class="form-control" id="sel1" required>
+                <select name="element_size" class="form-control" id="shape" required>
                     <option value="45" selected>45 degrees</option>
                     <option value="90">90 degrees</option>
                 </select>
                 <h5>Dimension:</h5>
                 <div class="row">
-                    <div class="col-md-4"><input type="number" min="1" step="1" placeholder="x" required/></div>
-                    <div class="col-md-4"><input type="number" min="1" step="1" placeholder="y"required/></div>
-                    <div class="col-md-4"><input type="number" min="1" step="1" placeholder="z" required/></div>
+                    <div class="col-md-4"><input id = "x" type="number" min="1" step="1" placeholder="x" required/></div>
+                    <div class="col-md-4"><input id = "y" type="number" min="1" step="1" placeholder="y"required/></div>
+                    <div class="col-md-4"><input id = "z" type="number" min="1" step="1" placeholder="z" required/></div>
                 </div>
                 <h5> </h5>
-                <input type="submit" class="btn btn-block btn-large" value="Generate" id="submit">
+                <input type="submit" class="btn btn-block btn-large" value="Generate" id="submit" onclick="calc_lattice();">
             </form>
         </div> 
     </row>
-    <div id="loader" hidden = "true"></div> 
+    <div id='loader' hidden="true"</div> 
 </body>
 </html>
 
--- app/view_mesh.php
+++ app/view_mesh.php
@@ -1,43 +1,13 @@
-<!DOCTYPE html>
-<html>
-	<?php
-		require ("includes/config.php");
-		if ($_SESSION["logged_in"] != "YES") {
-			header("Location: login.php");
-		}
-	?>
-
-	<head>
-		<script type='text/javascript' src='http://www.x3dom.org/download/x3dom.js'> </script> 
-		<link rel='stylesheet' type='text/css' href='http://www.x3dom.org/download/x3dom.css'></link>
-	  	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.0.min.js" ></script>
-
-	  	<script>	
-		  	function center(event)
-		  	{
-		  		document.getElementById('x3d_element').runtime.fitAll();
-		  	}
-	  	</script>
-	</head>
-
-
-	<body>
-		<div id="display">
-			<h1>3D Display of Mesh</h1>
-			<x3d id='x3d_element' class="x3d_element" align='center' style="left:30%;">
-				<div id="instructions">
-					<button type="button" onclick="center(event)" class="btn btn-secondary">Center</button>
-				</div>
-
-
-				<scene>
-					<viewpoint id = "angle1" position='45 0 200' orientation="0 0 1 0" description = "Cam Angle 1"></viewpoint>
-
-					<inline id="x3d_object" url="gmsh_output/<?php echo $_POST['id'];?>/mesh.x3d" onload="center();"></inline> 
-
-				</scene> 
-			</x3d>   
-		</div>
-
-	</body>
-</html>
+<?php
+	$id = $_POST["job_id"];
+    $element_size = $_POST["element_size"];
+    $max_element_size = $_POST["max_element_size"];
+    $min_element_size = $_POST["min_element_size"];
+    set_time_limit(240);
+	$val = "C:\Users\MD580\Desktop\Web-based-CAE-Cloud-Platform\app\scripts\check_msh\checkmesh.bat $id $element_size $max_element_size $min_element_size";
+	$output = exec($val);
+	$val1 = $py_path." py/allTomsh.py ".$id;
+    $output1 = exec($val1);
+    //print($output1);
+    echo json_encode(array('conversion'=>$output1));
+?> 
